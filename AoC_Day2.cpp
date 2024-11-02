c8126a515dcdd7daf592fa39664cd4c8aaf8abe1
Ngô Đức Hùng <115607252+ndhung18@users.noreply.github.com>
2024-11-02 23:48:38 +0700

    first 8 days

---
+++ AoC_Day2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ifstream file("D:\\Code\\VScode\\Advent of Code 2022\\AoC_Day2.txt");
+
+    if (file.is_open())
+    {
+        vector<string> arr;
+        string val;
+        while (getline(file, val))
+        {
+            if (file)
+                arr.push_back(val);
+            else
+                break;
+        }
+        vector<int> _arr;
+        int sum = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] == "A X")
+                sum += 3;
+            if (arr[i] == "A Y")
+                sum += 4;
+            if (arr[i] == "A Z")
+                sum += 8;
+            if (arr[i] == "B X")
+                sum += 1;
+            if (arr[i] == "B Y")
+                sum += 5;
+            if (arr[i] == "B Z")
+                sum += 9;
+            if (arr[i] == "C X")
+                sum += 2;
+            if (arr[i] == "C Y")
+                sum += 6;
+            if (arr[i] == "C Z")
+                sum += 7;
+        }
+        cout << sum;
+    }
+}
---
+++ AoC_Day3.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int value1(string s)
+{
+    int val;
+    string a, b;
+    a = s.substr(0, s.size() / 2);
+    b = s.substr(s.size() / 2, s.size() / 2);
+    for (int i = 0; i < a.size(); i++)
+    {
+        for (int j = 0; j < a.size(); j++)
+        {
+            if (a[i] == b[j])
+            {
+                if (a[i] >= 'a' && a[i] <= 'z')
+                {
+                    val = int(a[i]) - 96;
+                }
+                else
+                {
+                    val = int(a[i]) - 38;
+                }
+            }
+        }
+    }
+    return val;
+}
+
+int value2(string a, string b, string c)
+{
+    vector<char> item2;
+    int _val;
+    for (int i = 0; i < a.size(); i++)
+    {
+        for (int j = 0; j < b.size(); j++)
+        {
+            if (a[i] == b[j])
+            {
+                item2.push_back(a[i]);
+            }
+        }
+    }
+    sort(item2.begin(), item2.end());
+    item2.erase(unique(item2.begin(), item2.end()), item2.end());
+    // for (int i = 0; i < item2.size(); i++)
+    // {
+    //     cout << item2[i] << " ";
+    // }
+
+    for (int i = 0; i < item2.size(); i++)
+    {
+        for (int j = 0; j < c.size(); j++)
+        {
+            if (item2[i] == c[j])
+            {
+                if (c[j] >= 'a' && c[j] <= 'z')
+                {
+                    _val = int(c[j]) - 96;
+                }
+                else
+                {
+                    _val = int(c[j]) - 38;
+                }
+            }
+        }
+    }
+    return _val;
+}
+
+int main()
+{
+    ifstream file("D:\\Code\\VScode\\Advent of Code 2022\\AoC_Day3.txt");
+
+    vector<string> arr;
+    if (file.is_open())
+    {
+        string line;
+        while (getline(file, line))
+        {
+            if (file)
+                arr.push_back(line);
+            else
+                break;
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < arr.size(); i += 3)
+    {
+        sum += value2(arr[i], arr[i + 1], arr[i + 2]);
+    }
+    // sum += value2(arr[0], arr[1], arr[2]);
+    cout << sum;
+}
